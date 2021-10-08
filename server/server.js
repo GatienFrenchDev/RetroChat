bfee6f84b009da2c97e8d76bfe6dd3bd2525eebf
GatienFrenchDev <gatieng2005@gmail.com>
2021-10-08 21:47:38 +0200

    début du projet

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 80
+
+app.use('/css',express.static(__dirname +'/../client/css'));
+
+app.listen(port, () => {
+    console.log(`Lancement du serveur sur le port ${port}`)
+})
+
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname + "/../client/index.html"))
+})
