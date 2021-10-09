832141448eb78d32ec7ae1bb9eaadd79e9650d68
GatienFrenchDev <gatieng2005@gmail.com>
2021-10-09 17:23:22 +0200

    ca avance

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const express = require('express')
+const { cp } = require('fs')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
+
+const config = require('./config/config.json')
 
 const app = express()
-const port = 80
+const server = http.createServer(app)
+const io = socketio(server)
 
-app.use('/css',express.static(__dirname +'/../client/css'));
-app.use('/js',express.static(__dirname +'/../client/js'));
+const port = config.port
 
-app.listen(port, () => {
+app.use('/',express.static(__dirname +'/../client/'));
+
+server.listen(port, () => {
     console.log(`Lancement du serveur sur le port ${port}`)
 })
 
-
 app.get("/", (req, res) =>{
     res.sendFile(path.resolve(__dirname + "/../client/index.html"))
+})
+
+io.on('connection', socket =>{
+    console.log('NEW Ws connection...')
 })
