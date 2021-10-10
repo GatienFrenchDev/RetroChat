3e460ad4148fb1493a52338d2eaf316b2237e653
GatienFrenchDev <gatieng2005@gmail.com>
2021-10-10 10:16:39 +0200

    ca marche a peut pret

--- client/js/app.js
+++ client/js/app.js
@@ -1,25 +1,49 @@
-const socket = io()
-
-const formulaire = document.getElementById('message-form')
-
-
-socket.on('message', msg =>{
-    afficher(msg)
-})
-
-formulaire.addEventListener('submit', (e) =>{
-    e.preventDefault()
-    const msg = document.getElementById('msg').value
-    document.getElementById('msg').value = null
-    msg.trim()
-    socket.emit('NewMessage', msg)
-})
+const chatForm = document.getElementById('message-form') 
+const chatMessages = document.querySelector('.contenu') 
 
 
-function afficher(msg){
-    console.log(msg)
-    const div = document.createElement('div')
-    div.classList.add('message')
-    div.innerHTML = `<p class="pseudo">Gatien :</p><p class="contenu">${msg}</p>`
-    document.querySelector('.contenu').appendChild(div)
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+}) 
+
+const socket = io() 
+
+
+socket.emit('joinRoom', { username, room }) 
+
+
+socket.on('message', (message) => {
+  outputMessage(message) 
+
+  chatMessages.scrollTop = chatMessages.scrollHeight 
+}) 
+
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault() 
+
+
+  let msg = document.getElementById('msg').value
+
+  msg = msg.trim() 
+
+  if (!msg) {
+    return false 
+  }
+
+
+  socket.emit('chatMessage', msg)
+
+
+  document.getElementById('msg').value = null
+  document.getElementById('msg').focus()
+}) 
+
+
+function outputMessage(message) {
+  const div = document.createElement('div') 
+  div.classList.add('message') 
+  const p = document.createElement('p') 
+  div.innerHTML = `<p class="pseudo">${message.username} [${message.time}]:</p><p class="contenu">${message.text}</p>`
+  document.querySelector('.contenu').appendChild(div) 
 }
--- server/server.js
+++ server/server.js
@@ -5,34 +5,76 @@ const socketio = require('socket.io')
 
 const config = require('./config/config.json')
 
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
+
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
 const port = config.port
 
-app.use('/',express.static(__dirname +'/../client/'));
+app.use('/', express.static(__dirname + '/../client/'));
 
+const botName = config.nom_bot;
 
-io.on('connection', socket =>{
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-    socket.emit('message', 'Bienvenue sur le chat !')
+    socket.join(user.room);
 
-    socket.broadcast.emit('message', 'Un utilisateur a rejoint')
+    socket.emit('message', formatMessage(botName, 'Bienvenue sur RetroChat !'));
 
-    socket.on('disconnect', () =>{
-        io.emit('message', 'Un utilisateur a deconnecté')
-    })
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} a rejoint la salle`)
+      );
 
-    socket.on('NewMessage', (msg) =>{
-        io.emit('message', msg)
-    })
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} a quitté la salle`)
+      );
+
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      })
+    }
+  })
 })
 
 server.listen(port, () => {
     console.log(`Lancement du serveur sur le port ${port}`)
 })
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/../client/index.html"))
 })
